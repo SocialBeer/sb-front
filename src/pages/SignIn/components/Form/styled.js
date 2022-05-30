a8d1a4c682db52520afa8021487e599a87d4e1ef
n.semenchuk <n.semenchuk@itransition.com>
2022-05-30 18:33:57 +0300

    add lint-staged

--- src/pages/SignIn/components/Form/styled.js
+++ src/pages/SignIn/components/Form/styled.js
@@ -5,5 +5,6 @@ import { getUnitAsPixels } from '../../../../styles/sizes'
 export const FormContainer = styled.div`
   display: flex;
   flex-direction: column;
+  flex-direction: column;
   gap: ${getUnitAsPixels(3)};
 `
