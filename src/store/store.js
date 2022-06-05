466a985a29bc53b7de9e1b9ac7bc372169f87b46
n.semenchuk <n.semenchuk@itransition.com>
2022-06-05 17:23:32 +0300

    remove unused

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,4 @@
-import {
-  persistStore,
-  persistReducer,
-  REGISTER,
-  FLUSH,
-  REHYDRATE,
-  PURGE,
-  PERSIST,
-} from 'redux-persist'
+import { persistStore, persistReducer, PERSIST } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import {
   configureStore,
